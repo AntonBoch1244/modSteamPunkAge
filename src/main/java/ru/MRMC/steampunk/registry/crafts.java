b801472c5e72b45be13f72e27666e52b8654a24a
AntonBoch1244 <AntonBoch12.44@gmail.com>
2014-11-06 23:31:51 +0400

    Getting ready Crafting and remove Minecraft Version in reference
    Signed-off-by: AntonBoch1244 <AntonBoch12.44@gmail.com>

--- src/main/java/ru/MRMC/steampunk/registry/crafts.java
+++ src/main/java/ru/MRMC/steampunk/registry/crafts.java
@@ -5,8 +5,26 @@ import net.minecraft.item.ItemStack;
 
 public class crafts
 {
-    public static void register()
+    static void workbench()
+    {
+        //this for crafting
+    }
+    static void fuel()
+    {
+        //this for fuel
+    }
+    static void smelting()
     {
         GameRegistry.addSmelting(blocks.copperOre, new ItemStack(items.copperIngot), 0.2F);
+    }
+    static void furnace()
+    {
+        fuel();
+        smelting();
+    }
+    public static void register()
+    {
+        workbench();
+        furnace();
     }
 }
